9b71c85b717ac8a841a8976c6ba6f1b1bd9c4c00
danshouyaak2 <113924712@qq.com>
2023-09-08 19:03:37 +0800

    登录页面

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,8 +1,45 @@
+import { useEffect } from "react"
 import style from "./login.module.scss"
+import intitLoginBg from "./init.ts"
+import { Input, Space, Button } from "antd"
+//import 'antd/dist/antd.less'
+import './login.less'
+
 const view = () => {
+    //加载完组件之后,加载背景
+    useEffect(() => {
+        intitLoginBg();
+        window.onresize = function () {
+            intitLoginBg()
+            //窗口的改变引起初始化
+        }
+    }, [])
     return (
-        <div className = {style.loginPage}>
-            这是login的登录页
+        // 登陆盒子
+        <div className={style.loginPage}>
+
+            {/* 存放背景 */}
+            <canvas id="canvas" style={{ display: "block" }}></canvas>
+
+            <div className={style.loginBox + " loginBox"}>
+            {/* 辅助类 空格加类名 */}
+
+                {/* 登录盒子 */}
+
+                <div className={style.title}>
+                    {/* 登录标题 */}
+                    <h1>----------</h1>
+                    <p>Strive Everyday</p>
+                </div>
+                {/* 表单部分 */}
+                <div className="form">
+                    <Space direction="vertical" size="large" style={{ display: 'flex' }}>
+                        <Input placeholder="请输入用户名："></Input>
+                        <Input.Password placeholder="请输入密码："></Input.Password>
+                        <Button type="primary" block>登录</Button>
+                    </Space>
+                </div>
+            </div>
         </div>
     )
 }
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,3 @@
+// 模块引入声明
 /// <reference types="vite/client" />
+declare module "*.ts"
