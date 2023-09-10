6f8bb95885df8db9216283453952cf725893effd
danshouyaak2 <113924712@qq.com>
2023-09-10 15:34:09 +0800

    登录界面设置

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,9 +1,10 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import style from "./login.module.scss"
 import intitLoginBg from "./init.ts"
 import { Input, Space, Button } from "antd"
 //import 'antd/dist/antd.less'
 import './login.less'
+import { log } from "console"
 
 const view = () => {
     //加载完组件之后,加载背景
@@ -14,6 +15,15 @@ const view = () => {
             //窗口的改变引起初始化
         }
     }, [])
+
+    //获取用户输入的信息
+    const [usenameVal,set] = useState("") //存储用户输入的信息
+    const usernameChange = (e: ChangeEvent<HTMLInputElement>) => {
+        //获取用户输入的用户名
+        console.log(e.target.value);
+
+    }
+
     return (
         // 登陆盒子
         <div className={style.loginPage}>
@@ -22,7 +32,7 @@ const view = () => {
             <canvas id="canvas" style={{ display: "block" }}></canvas>
 
             <div className={style.loginBox + " loginBox"}>
-            {/* 辅助类 空格加类名 */}
+                {/* 辅助类 空格加类名 */}
 
                 {/* 登录盒子 */}
 
@@ -34,9 +44,15 @@ const view = () => {
                 {/* 表单部分 */}
                 <div className="form">
                     <Space direction="vertical" size="large" style={{ display: 'flex' }}>
-                        <Input placeholder="请输入用户名："></Input>
+                        <Input placeholder="请输入用户名：" onChange={usernameChange}></Input>
                         <Input.Password placeholder="请输入密码："></Input.Password>
-                        <Button type="primary" block>登录</Button>
+                        <div className="captchaBox">
+                            <Input placeholder="请输入验证码：" ></Input>
+                            <div className="captchaImg">
+                                <img src="" alt="" height="38" />
+                            </div>
+                        </div>
+                        <Button type="primary" block className="loginBtn">登录</Button>
                     </Space>
                 </div>
             </div>
